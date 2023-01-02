39466cc5bc6caebbb8fce126e457419192fed05f
MarianaPenaAntuna <marianapenamovil@gmail.com>
2023-01-02 19:12:21 -0300

    020120231909

--- src/main/java/com/portafolio/MarianaPena/controller/ExperienciaController.java
+++ src/main/java/com/portafolio/MarianaPena/controller/ExperienciaController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin()
 @RequestMapping("/experiencias")
 public class ExperienciaController {
 
